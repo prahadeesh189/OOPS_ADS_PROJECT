756b1aca14ba8d0ef31ebe5d35b6925353a13c4a
Prahadeesh K <60143336+prahadeesh189@users.noreply.github.com>
2021-05-28 14:50:07 +0530

    delete with space

--- contactBook.cpp
+++ contactBook.cpp
@@ -182,65 +182,321 @@ void SplayTree::inOrderDisplayWrapper() {
 
 
 
-int main() {
-
-    long int kee;
-    int choice;
-
-    // start
-    clone();
 
 
 
-    while (true) {
 
-        cout << "\n****** MENU ******\n1 - Add New Contact\n2 - Delete Contact\n3 - Get Info\n4 - All Contacts\n";
-        cout << "\nchoice: ";
-        cin >> choice; 
 
-        switch (choice) {
-            case 1:
-                cout << "\n--- New Contact ---" << endl;
-                CreateNewContact();
-                contactSplTree.preOrderWriteWrapper();
-                break;
-            case 2:
-                cout << "Enter Delete Number: ";
-                cin >> kee;
-                deleteContact(kee);
-                break;
-            case 3:
-                cout << "Enter Number: ";
-                cin >> kee;
-                getInfo(kee);
-                break;
-            case 4:
-                cout << "\n------------\n";
-                cout << "\nAll Contacts\n";
-                // contactSplTree.inOrderWrapper();
-                contactSplTree.inOrderDisplayWrapper();
 
-                cout << "------------\n";
-                break;
-            case 5:
-                cout << "\n------------\n";
-                contactSplTree.displayWrapper();
-                cout << "------------\n";
-                
-                break;
-            case 0:
-                cout << "\n EXITING..." << endl;
-                // end
-                contactSplTree.preOrderWriteWrapper();
 
-                exit(1);
-                break;
+
+
+
+
+
+void deleteContact( long int numberDel) {
+
+    int rslt =  contactSplTree.del(numberDel);
+
+    if (rslt != 1) {
+        cout << "\nContact does not exists...\n";
+        return;
+    }
+
+
+    ifstream fin_Data;
+    fin_Data.open( dataDBPath , ios::app );
+    
+
+    ofstream fout_tempData;
+    fout_tempData.open ( tempDataDBPath , std::ios::trunc);
+    
+
+    
+    bool afterFlag = false;
+
+    while (fin_Data.eof() == false) {
+        string line;
+        getline(fin_Data , line);
+        cout << line << endl;
+
+
+
+        if (line.length() != 0) {
             
-            default:
-                cout << "\n INVALID CHOICE..." << endl;
-                break;
+
+            string inde = "", number = "";
+    
+            // index
+            int i=0;
+            for (; i<line.length(); i++) {
+                if (line[i] == '|') {
+                    break;
+                }
+
+                inde += line[i];
+            }
+            i++;
+            
+            // name
+            for (; i<line.length(); i++) {
+                if (line[i] == '|') {
+                    break;
+                }
+            }
+            i++;
+
+            // number
+            for (; i<line.length(); i++) {
+                if (line[i] == '|') {
+                    break;
+                }
+
+                number += line[i];
+            }
+            i++;
+
+
+
+
+
+            stringstream string_TO_int_stream;
+            int ind;
+            string_TO_int_stream << inde;
+            string_TO_int_stream >> ind;
+
+
+
+            stringstream string_TO_int_stream_number;
+            long int numb;
+            string_TO_int_stream_number << number;
+            string_TO_int_stream_number >> numb;
+
+
+            if (numb == numberDel ) {
+                afterFlag  = true;
+                continue;
+            }
+
+
+
+            if (afterFlag != 1) {
+                fout_tempData << line << "\n";
+
+            }
+            else {
+                string stringIndex = "";
+
+                // index
+                int i=0;
+                for (; i<line.length(); i++) {
+                    if (line[i] == '|') {
+                        break;
+                    }
+
+                    stringIndex += line[i];
+                }
+                i++;
+
+
+
+                stringstream string_TO_int_stream_ind;
+                int index_1;
+                string_TO_int_stream_ind << stringIndex;
+                string_TO_int_stream_ind >> index_1;
+                index_1--;
+
+
+                string ind_string;
+
+                stringstream int_TO_string_stream;  
+                int_TO_string_stream << index_1;   
+                int_TO_string_stream >> ind_string;
+
+
+
+
+                Contact ctp = toContact(line);
+
+                string dtLine = "";
+
+                dtLine += ind_string;
+                dtLine += "|";
+                dtLine += ctp.toString();
+
+                fout_tempData << dtLine;
+            }
+
+        }
+        
+    };
+
+
+    fout_tempData.close();
+    fin_Data.close();
+
+
+
+    
+    ofstream fout_Data;
+    fout_Data.open( dataDBPath , std::ios::trunc );
+    
+
+    ifstream fin_tempData;
+    fin_tempData.open ( tempDataDBPath , ios::app);
+
+
+
+
+    while (fin_tempData.eof() == false) {
+        string line;
+        getline(fin_tempData , line);
+
+        if (line.length() != 0) {
+            fout_Data << line << endl;
         }
     }
+
+    fin_tempData.close();
+    fout_Data.close();
+
+
+
+
+    ifstream fin_metaData;
+    fin_metaData.open ( metaDataDBPath ,ios::app);
+
+    int count;
+    fin_metaData >> count;
+    fin_metaData.close();
+
+
+    ofstream fout_metaData;
+    fout_metaData.open ( metaDataDBPath , std::ios::trunc);
+        
+    fout_metaData << count-1;
+    fout_metaData.close();
+}
+
+
+
+
+
+
+
+int main() {
+
+
+
+
+    contactSplTree.clone(1234567006, 8);
+    contactSplTree.clone(1234567005, 5);
+    contactSplTree.clone(1234567004, 4);
+    contactSplTree.clone(1234567003, 3);
+    contactSplTree.clone(1234567002, 2);
+    contactSplTree.clone(1234567001, 1);
+    contactSplTree.clone(1234567007, 6);
+    contactSplTree.clone(1234567008, 7);
+
+
+    // contactSplTree.displayWrapper();
+
+
+
+
+
+
+    deleteContact( 1234567005 );
+
+
+    cout << "\n\n\n";
+    // contactSplTree.displayWrapper();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // long int kee;
+    // int choice;
+
+    // // start
+    // clone();
+
+
+
+    // while (true) {
+
+    //     cout << "\n****** MENU ******\n1 - Add New Contact\n2 - Delete Contact\n3 - Get Info\n4 - All Contacts\n";
+    //     cout << "\nchoice: ";
+    //     cin >> choice; 
+
+    //     switch (choice) {
+    //         case 1:
+    //             cout << "\n--- New Contact ---" << endl;
+    //             CreateNewContact();
+    //             contactSplTree.preOrderWriteWrapper();
+    //             break;
+    //         case 2:
+    //             cout << "Enter Delete Number: ";
+    //             cin >> kee;
+    //             deleteContact(kee);
+    //             break;
+    //         case 3:
+    //             cout << "Enter Number: ";
+    //             cin >> kee;
+    //             getInfo(kee);
+    //             break;
+    //         case 4:
+    //             cout << "\n------------\n";
+    //             cout << "\nAll Contacts\n";
+    //             // contactSplTree.inOrderWrapper();
+    //             contactSplTree.inOrderDisplayWrapper();
+
+    //             cout << "------------\n";
+    //             break;
+    //         case 5:
+    //             cout << "\n------------\n";
+    //             contactSplTree.displayWrapper();
+    //             cout << "------------\n";
+                
+    //             break;
+    //         case 0:
+    //             cout << "\n EXITING..." << endl;
+    //             // end
+    //             contactSplTree.preOrderWriteWrapper();
+
+    //             exit(1);
+    //             break;
+            
+    //         default:
+    //             cout << "\n INVALID CHOICE..." << endl;
+    //             break;
+    //     }
+    // }
 
 
 
@@ -464,191 +720,6 @@ void CreateNewContact() {
 
     cout << "\nNew Contact Addedd Successfully\n";
 };
-
-
-void deleteContact( long int numberDel) {
-
-    int rslt =  contactSplTree.del(numberDel);
-
-    if (rslt != 1) {
-        cout << "\nContact does not exists...\n";
-        return;
-    }
-
-
-    ifstream fin_Data;
-    fin_Data.open( dataDBPath , ios::app );
-    
-
-    ofstream fout_tempData;
-    fout_tempData.open ( tempDataDBPath , std::ios::trunc);
-    
-
-    
-    bool afterFlag = false;
-
-    while (fin_Data.eof() == false) {
-        string line;
-        fin_Data >> line;
-
-
-
-
-        if (line.length() != 0) {
-            
-
-            string inde = "", number = "";
-    
-            // index
-            int i=0;
-            for (; i<line.length(); i++) {
-                if (line[i] == '|') {
-                    break;
-                }
-
-                inde += line[i];
-            }
-            i++;
-            
-            // name
-            for (; i<line.length(); i++) {
-                if (line[i] == '|') {
-                    break;
-                }
-            }
-            i++;
-
-            // number
-            for (; i<line.length(); i++) {
-                if (line[i] == '|') {
-                    break;
-                }
-
-                number += line[i];
-            }
-            i++;
-
-
-
-
-
-            stringstream string_TO_int_stream;
-            int ind;
-            string_TO_int_stream << inde;
-            string_TO_int_stream >> ind;
-
-
-
-            stringstream string_TO_int_stream_number;
-            long int numb;
-            string_TO_int_stream_number << number;
-            string_TO_int_stream_number >> numb;
-
-
-            if (numb == numberDel ) {
-                afterFlag  = true;
-                continue;
-            }
-
-
-
-            if (afterFlag != 1) {
-                fout_tempData << line << "\n";
-            }
-            else {
-                string stringIndex = "";
-
-                // index
-                int i=0;
-                for (; i<line.length(); i++) {
-                    if (line[i] == '|') {
-                        break;
-                    }
-
-                    stringIndex += line[i];
-                }
-                i++;
-
-
-
-                stringstream string_TO_int_stream_ind;
-                int index_1;
-                string_TO_int_stream_ind << stringIndex;
-                string_TO_int_stream_ind >> index_1;
-                index_1--;
-
-
-                string ind_string;
-
-                stringstream int_TO_string_stream;  
-                int_TO_string_stream << index_1;   
-                int_TO_string_stream >> ind_string;
-
-
-
-
-                Contact ctp = toContact(line);
-
-                string dtLine = "";
-
-                dtLine += ind_string;
-                dtLine += "|";
-                dtLine += ctp.toString();
-
-                fout_tempData << dtLine;
-            }
-
-        }
-        
-    };
-
-
-    fout_tempData.close();
-    fin_Data.close();
-
-
-
-    
-    ofstream fout_Data;
-    fout_Data.open( dataDBPath , std::ios::trunc );
-    
-
-    ifstream fin_tempData;
-    fin_tempData.open ( tempDataDBPath , ios::app);
-
-
-
-
-    while (fin_tempData.eof() == false) {
-        string line;
-        fin_tempData >> line;
-
-        if (line.length() != 0) {
-            fout_Data << line << endl;
-        }
-    }
-
-    fin_tempData.close();
-    fout_Data.close();
-
-
-
-
-    ifstream fin_metaData;
-    fin_metaData.open ( metaDataDBPath ,ios::app);
-
-    int count;
-    fin_metaData >> count;
-    fin_metaData.close();
-
-
-    ofstream fout_metaData;
-    fout_metaData.open ( metaDataDBPath , std::ios::trunc);
-        
-    fout_metaData << count-1;
-    fout_metaData.close();
-
-}
 
 
 
